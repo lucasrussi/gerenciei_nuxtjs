fc9d20e156d51241aea411c959644fd8d2e45908
lucasrussi <60052471+lucasrussi@users.noreply.github.com>
2022-06-25 11:29:22 -0300

    Add files via upload

---
+++ dist/_nuxt/static/1656164075/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fgerenciei_nuxtjs\u002F_nuxt\u002Fstatic\u002F1656164075",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fgerenciei_nuxtjs\u002F",assetsPath:"\u002Fgerenciei_nuxtjs\u002F_nuxt\u002F",cdnURL:a}}}}(null));
